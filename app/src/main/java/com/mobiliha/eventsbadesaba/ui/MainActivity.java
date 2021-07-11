75f6058d27fa23ccd25dab047d00649807ac2316
Sajjad Roudi <sajjad.roodi@gmail.com>
2021-07-11 16:37:29 +0430

    Create packages based on MVVM arch

--- app/src/main/java/com/mobiliha/eventsbadesaba/ui/MainActivity.java
+++ app/src/main/java/com/mobiliha/eventsbadesaba/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mobiliha.eventsbadesaba;
+package com.mobiliha.eventsbadesaba.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mobiliha.eventsbadesaba.R;
 
 public class MainActivity extends AppCompatActivity {
 
