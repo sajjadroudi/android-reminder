8d30327011706a5d0348fa4bc33ef3092227f8f0
Sajjad Roudi <sajjad.roodi@gmail.com>
2021-07-15 16:22:16 +0430

    Force farsi as default language

--- app/src/main/java/com/mobiliha/eventsbadesaba/ui/MainActivity.java
+++ app/src/main/java/com/mobiliha/eventsbadesaba/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mobiliha.eventsbadesaba.ui;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,8 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        forceRtlIfSupported();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void forceRtlIfSupported() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        }
     }
 
     @Override
