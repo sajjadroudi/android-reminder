dda5b4c9543a070a92182570ecd1137bc8d7c4d3
Sajjad Roudi <sajjad.roodi@gmail.com>
2021-07-26 20:05:29 +0430

    Remove unnecessary custom edit text custom view

---
+++ app/src/main/java/com/mobiliha/eventsbadesaba/util/ViewUtils.java
@@ -0,0 +1,11 @@
+package com.mobiliha.eventsbadesaba.util;
+
+import android.widget.EditText;
+
+public class ViewUtils {
+
+    public static void makeEmpty(EditText editText) {
+        editText.setText(null);
+    }
+
+}
