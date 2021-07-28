c6c3ba9ee55cf94c54d3438df3fd4fc93b6cdc7b
Sajjad Roudi <sajjad.roodi@gmail.com>
2021-07-28 16:36:49 +0430

    Remove duplicates code for selecting task color

--- app/src/main/java/com/mobiliha/eventsbadesaba/util/BindingUtils.java
+++ app/src/main/java/com/mobiliha/eventsbadesaba/util/BindingUtils.java
@@ -1,11 +1,16 @@
 package com.mobiliha.eventsbadesaba.util;
 
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import androidx.databinding.BindingAdapter;
 
+import com.mobiliha.eventsbadesaba.R;
 import com.mobiliha.eventsbadesaba.data.local.db.entity.TaskColor;
 
 public class BindingUtils {
@@ -21,6 +26,32 @@ public class BindingUtils {
                     taskColor.getColorCode(), PorterDuff.Mode.SRC_ATOP
             );
         }
+    }
+
+    @BindingAdapter("taskColor")
+    public static void setTaskColor(RadioButton radioButton, TaskColor taskColor) {
+        if(taskColor == null)
+            return;
+
+        GradientDrawable checkedShape = new GradientDrawable();
+        checkedShape.setShape(GradientDrawable.OVAL);
+        checkedShape.setColor(taskColor.getColorCode());
+        int strokeWidth = (int) radioButton.getResources().getDimension(
+                R.dimen.task_modify_colored_radio_button_stroke_width
+        );
+        checkedShape.setStroke(strokeWidth, Color.BLACK);
+
+        GradientDrawable uncheckedShape = new GradientDrawable();
+        uncheckedShape.setShape(GradientDrawable.OVAL);
+        uncheckedShape.setColor(taskColor.getColorCode());
+
+        StateListDrawable drawable = new StateListDrawable();
+        drawable.addState(
+                new int[]{android.R.attr.state_checked}, checkedShape
+        );
+        drawable.addState(new int[]{}, uncheckedShape);
+
+        radioButton.setBackground(drawable);
     }
 
 }
